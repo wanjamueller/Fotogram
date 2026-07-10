74cbc69ad9e75764509cbab821b1b49aba8b4080
wanjamueller <me@wanjamueller.com>
2026-07-10 20:12:34 +0200

    final touches v5

--- script.js
+++ script.js
@@ -1,67 +1,3 @@
-// let myImages = [
-//     {
-//         alt: "Paper speech bubble reading 'FOOL!' taped to a red corrugated metal shutter",
-//         file: "atanaspaskalev-fool.jpg",
-//         name: "atanaspaskalev-fool",
-//     },
-//     {
-//         alt: "Snow-covered rolling hills at sunset with dramatic clouds and a low sun",
-//         file: "edyttka1388-winter-2.jpg",
-//         name: "edyttka1388-winter-2",
-//     },
-//     {
-//         alt: "Snow-covered field and hills backlit by a bright sun near the horizon",
-//         file: "edyttka1388-winter.jpg",
-//         name: "edyttka1388-winter",
-//     },
-//     {
-//         alt: "Close-up of a weathered blue wooden edge with peeling paint and exposed",
-//         file: "engin_akyurt-blue.jpg",
-//         name: "engin_akyurt-blue",
-//     },
-//     {
-//         alt: "Old worndown brickwall with streetart",
-//         file: "engin_akyurt-stone.jpg",
-//         name: "engin_akyurt-stone",
-//     },
-//     {
-//         alt: "Close-up of rough dark grey stacked stones forming a wall",
-//         file: "freeimagesguide-rock-wall.jpg",
-//         name: "freeimagesguide-rock-wall",
-//     },
-//     {
-//         alt: "Layered stickers and posters on a wall, including one reading 'Yes, life is good'",
-//         file: "mariya_m-poster.jpg",
-//         name: "mariya_m-poster",
-//     },
-//     {
-//         alt: "Horse on a lead rope standing on wet ground with snowy hills in the background",
-//         file: "mellyschatz321-pferd.jpg",
-//         name: "mellyschatz321-pferd",
-//     },
-//     {
-//         alt: "Close-up of rippled sand dune patterns in warm light",
-//         file: "mostafa_meraji-desert.jpg",
-//         name: "mostafa_meraji-desert",
-//     },
-//     {
-//         alt: "Weathered blue wooden boards with peeling paint revealing bare wood",
-//         file: "no-longer-here-peeling.jpg",
-//         name: "no-longer-here-peeling",
-//     },
-//     {
-//         alt: "Street art on a cracked wall showing a boy flying a kite shaped like a monster",
-//         file: "tho-ge-street-art.jpg",
-//         name: "tho-ge-street-art",
-//     },
-//     { alt: "Rain droplets on a glass surface", file: "tlcmich-sunroof.jpg", name: "tlcmich-sunroof" },
-//     {
-//         alt: "Extreme close-up of a tabby cat's face, focused on its green eye and whiskers",
-//         file: "ykaiavu-cat.jpg",
-//         name: "ykaiavu-cat",
-//     },
-// ];
-
 // #region link HTML
 
 const containerRef = document.getElementById("container");
@@ -105,20 +41,33 @@ function closeFocus() {
     dialogRef.classList.remove("open");
 }
 
-function init() {
+function eventListeners() {
     dialogRef.addEventListener("click", (event) => {
         if (event.target === dialogRef) {
             closeFocus();
         }
     });
 
+    // dialogRef.addEventListener("click", (event) => {
+    //     if (event.target !== dialogRef) {
+    //         eventBubblingPrevention();
+    //     }
+    // });
+
     dialogRef.addEventListener("keydown", (esc) => {
         if (esc.key === "Escape") {
             closeFocus();
         }
     });
+}
 
+function eventBubblingPrevention(event) {
+    event.stopPropagation();
+}
+
+function init() {
     renderPictures();
+    eventListeners();
 }
 
 // #endregion functions
--- scripts/templates.js
+++ scripts/templates.js
@@ -7,12 +7,12 @@ function framesTemplate(i) {
 
 function dialogTemplate(i) {
     return /*html*/ `
-    <div aria-labelledby="dialogTitle" class="dialog">
+    <div onclick="eventBubblingPrevention(event)" aria-labelledby="dialogTitle" class="dialog">
         <header class="dialog-header">
             <h2 tabindex="0" aria-describedby="picture name" id="dialogTitle">${myImages[i].name}</h2>
             <div class="close"><button tabindex="0" aria-label="close dialog" class="close-button" onclick="closeFocus()" onkeypress="closeFocus()" ><img src="./assets/icons/close_x.png" alt="click X to close the popup"></button></div>
         </header>
-            <figure class="focus-picture"><img class="picture-open" id="pic${i}" src="./assets/img/${myImages[i].file}" alt="./assets/img/${myImages[i].alt}" onclick="focusPicture(i)"></figure>
+            <figure class="focus-picture"><img class="picture-open" id="pic${i}" src="./assets/img/${myImages[i].file}" alt="./assets/img/${myImages[i].alt}" onclick="focusPicture(${i})"></figure>
         <footer class="dialog-footer">
             <button tabindex="0" aria-label="next picture" onclick="prevPic(${i})" onkeypress="prevPic(${i})" ><img src="./assets/icons/arrow_left.png" alt="arrow left to previous picture"></button>
             <div><p>${i + 1}/${myImages.length}</p></div>
